8ec2b7f5e14983db63bef213b155f07b04e54dec
Wilakwim <eljuligarzon@hotmail.com>
2024-10-22 10:28:33 -0500

    creacion de la estructura del proyecto y las clases model

---
+++ src/main/java/com/tienda/flores/model/Usuario.java
@@ -0,0 +1,93 @@
+package com.tienda.flores.model;
+
+public class Usuario {
+	
+	private Integer id;
+	private String nombre;
+	private String Username;
+	private String email;
+	private String direccion;
+	private String telefono;
+	private String tipo;
+	private String password;
+	
+	public Usuario() {
+		
+	}
+	
+	
+	
+	public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono,
+			String tipo, String password) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		Username = username;
+		this.email = email;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.tipo = tipo;
+		this.password = password;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", Username=" + Username + ", email=" + email
+				+ ", direccion=" + direccion + ", telefono=" + telefono + ", tipo=" + tipo + ", password=" + password
+				+ "]";
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/com/tienda/flores/TiendaFloresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.flores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaFloresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
