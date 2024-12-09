517c503108825c3f99db43eddc66b11411b1729b
Wilakwim <eljuligarzon@hotmail.com>
2024-12-09 12:04:59 -0500

    Funcion mostrar productos para el Administrador

--- src/main/java/com/tienda/flores/service/ProductoService.java
+++ src/main/java/com/tienda/flores/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.tienda.flores.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.tienda.flores.model.Producto;
@@ -11,6 +12,8 @@ public interface ProductoService {
 	public Optional<Producto> get(Integer id);
 	public void update(Producto producto);
 	public void delete(Integer id);
+	public List<Producto> findAll();
+	
 	
 	
 
