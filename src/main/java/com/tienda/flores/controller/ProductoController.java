c4f6512fda61848f58c93ea23c2f3a125763d9bb
Wilakwim <eljuligarzon@hotmail.com>
2024-12-12 15:36:43 -0500

    Implementacion para guardar la img del producto

--- src/main/java/com/tienda/flores/controller/ProductoController.java
+++ src/main/java/com/tienda/flores/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package com.tienda.flores.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.tienda.flores.model.Producto;
 import com.tienda.flores.model.Usuario;
 import com.tienda.flores.service.ProductoService;
+import com.tienda.flores.service.UploadFileService;
 
 @Controller
 @RequestMapping("/productos")
@@ -23,6 +27,10 @@ public class ProductoController {
 	
 	@Autowired
 	private ProductoService productoService;
+	
+	@Autowired
+	private UploadFileService upload;
+	
 	
 	
 	@GetMapping("")
@@ -40,10 +48,30 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/save")
-    public String save(Producto producto) {
+    public String save(Producto producto, @RequestParam("img")MultipartFile file) throws IOException {
     	LOGGER.info("Este es el objeto producto {}",producto);
     	Usuario u= new Usuario(1, "", "", "", "", "", "", "");
     	producto.setUsuario(u);
+    	
+    	
+    	//imagen
+    	if (producto.getId()==null) { // cuando se crea un producto 
+    	    String nombreImagen= upload.saveImage(file);
+    	    producto.setImagen(nombreImagen);
+    	}else {
+    		if (file.isEmpty()) { // cuando se edita el producto
+    			Producto p= new Producto();
+    			p=productoService.get(producto.getId()).get();
+    			producto.setImagen(p.getImagen());
+    			
+    		}else {
+    			
+    			String nombreImagen= upload.saveImage(file);
+        	    producto.setImagen(nombreImagen);
+    			
+    	}
+    	}
+    	
     	productoService.save(producto);
     	return "redirect:/productos";
     	
--- src/main/java/com/tienda/flores/service/UploadFileService.java
+++ src/main/java/com/tienda/flores/service/UploadFileService.java
@@ -1,5 +1,41 @@
 package com.tienda.flores.service;
 
-public class UploadFileService {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadFileService {
+	private String folder="imgflores//";
+	
+		
+		public String saveImage(MultipartFile file) throws IOException  {
+			if (!file.isEmpty()) {
+				byte [] bytes=file.getBytes();
+				
+				Path path =Paths.get(folder+file.getOriginalFilename());
+				Files.write(path, bytes);
+				return file.getOriginalFilename();
+			}
+		
+		return "default.jpg";
+		
+	}
+
+	    public void deleteImage(String nombre) {
+	    	String ruta="imgflores//";
+	    	File file=new File(ruta+nombre);
+	    	file.delete();
+	    	
+	    	
+	    	
+	    	
+	    }   
 }
+
+
