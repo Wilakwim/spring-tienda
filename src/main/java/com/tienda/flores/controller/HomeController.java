c434f28fa673f1923ea4289e27a86c7e1097c9d8
Wilakwim <eljuligarzon@hotmail.com>
2025-01-23 12:17:23 -0500

    Añadir nuevos productos al carrito de compras y mostrarlos en la vista carrito

--- src/main/java/com/tienda/flores/controller/HomeController.java
+++ src/main/java/com/tienda/flores/controller/HomeController.java
@@ -75,6 +75,8 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio() * cantidad);
 		detalleOrden.setProducto(producto);
 		
+		detalles.add(detalleOrden);
+		
 		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
 		orden.setTotal(sumaTotal);
