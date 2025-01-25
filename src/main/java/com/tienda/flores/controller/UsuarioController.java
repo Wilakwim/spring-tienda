eca38a770462d1469e60c6bbdf567888a4a2d6dc
Wilakwim <eljuligarzon@hotmail.com>
2025-01-25 09:50:46 -0500

    se creo funcionalidad registro usuario

---
+++ src/main/java/com/tienda/flores/controller/UsuarioController.java
@@ -0,0 +1,41 @@
+package com.tienda.flores.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tienda.flores.model.Usuario;
+import com.tienda.flores.service.IUsuarioService;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+	private final Logger logger= LoggerFactory.getLogger(UsuarioController.class);
+	
+	
+	
+	@Autowired
+	private IUsuarioService usuarioService;
+	
+	
+	// /usuario/registro
+	@GetMapping("/registro")
+	public String create() {
+		return "usuario/registro";
+	}
+	
+	@PostMapping("/save")
+	public String save(Usuario usuario) {
+		logger.info("Usuario registro: {}", usuario);
+		usuario.setTipo("USER");
+		usuarioService.save(usuario);
+		return "redirect:/";
+	
+}
+	
+}
