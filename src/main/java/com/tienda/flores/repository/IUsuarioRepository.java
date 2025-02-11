3c65aba9a1d7b5f150c1989a4073c0b462c937a4
Wilakwim <eljuligarzon@hotmail.com>
2025-02-11 14:48:18 -0500

    Se Obtiene un Usuario Login

--- src/main/java/com/tienda/flores/repository/IUsuarioRepository.java
+++ src/main/java/com/tienda/flores/repository/IUsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.tienda.flores.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,9 @@ import com.tienda.flores.model.Usuario;
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer>{
+	Optional<Usuario> findByEmail(String email);
 
 }
 
+
+	
